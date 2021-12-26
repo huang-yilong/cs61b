8ea9bbdd833e8737167dd1e75f0706bd537bfb98
huangyilong2000 <304080325@qq.com>
2021-12-26 19:25:46 +0800

    proj1

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,73 @@
+public class ArrayDeque<T> {
+    private T[] items;
+    private int size;
+
+    public ArrayDeque(){
+        items = (T[]) new Object[8];
+        size = 0;
+    }
+
+    public ArrayDeque(ArrayDeque<T> other){
+        items = (T[])new Object[other.size()];
+        System.arraycopy(other.items,0,items,0,size);
+        size = other.size();
+    }
+
+    public void addFirst(T item){
+        if(size == items.length)
+            resize(size+1,0,1,size);
+        else
+            resize(items.length,0,1,size);
+        items[0]=item;
+        size++;
+    }
+
+    private void resize(int capacity,int srcPos,int desPos,int len){
+        T[] a = (T[])new Object[capacity];
+        System.arraycopy(items,srcPos,a,desPos,len);
+        items = a;
+    }
+
+    public void addLast(T item){
+        if(size == items.length){
+            resize(size+1,0,0,size);
+        }
+        items[size]=item;
+        size++;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        for(int i=0;i<size;++i){
+            System.out.print(items[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public T removeFirst(){
+        if(isEmpty()) return null;
+        T item = items[0];
+        resize(size-1,1,0,size-1);
+        size--;
+        return item;
+    }
+
+    public T removeLast(){
+        if(isEmpty())return null;
+        T item = items[--size];
+        return item;
+    }
+
+    public T get(int index){
+        if(index>=size) return null;
+        return items[index];
+    }
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,97 @@
+public class LinkedListDeque<T> {
+    private class Node{
+        public T item;
+        public Node prev;
+        public Node next;
+        public Node(T i){
+            item = i;
+        }
+    }
+
+    private Node sentinal;
+    private int size;
+
+    public LinkedListDeque(){
+        sentinal = new Node(null);
+        sentinal.prev=sentinal;
+        sentinal.next=sentinal;
+        size = 0;
+    }
+
+    public LinkedListDeque(LinkedListDeque<T> other){
+        sentinal = new Node(null);
+        sentinal.prev = sentinal;
+        sentinal.next = sentinal;
+        size=0;
+
+        for(int i=0;i<other.size(); i++){
+            addLast(other.get(i));
+        }
+    }
+
+    public void addFirst(T item) {
+        Node n = new Node(item);
+        n.next=sentinal.next;
+        sentinal.next.prev=n;
+        n.prev=sentinal;
+        sentinal.next=n;
+        size++;
+    }
+
+    public void addLast(T item){
+        Node n = new Node(item);
+        n.prev=sentinal.prev;
+        sentinal.prev.next=n;
+        n.next=sentinal;
+        sentinal.prev=n;
+        size++;
+    }
+
+    public T removeFirst(){
+        if(isEmpty()) return null;
+        T item = sentinal.next.item;
+        sentinal.next=sentinal.next.next;
+        sentinal.next.prev=sentinal;
+        size--;
+        return item;
+    }
+
+    public T removeLast(){
+        if(isEmpty()) return null;
+        T item = sentinal.prev.item;
+        sentinal.prev=sentinal.prev.prev;
+        sentinal.prev.next=sentinal;
+        size--;
+        return item;
+    }
+
+    public boolean isEmpty(){
+        return sentinal.next==sentinal;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        if (isEmpty()){
+            System.out.println();
+            return;
+        }
+        Node cur = sentinal.next;
+        while(cur != sentinal){
+            System.out.print(cur.item+" ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    public T get(int index){
+        if(index>=size) return null;
+        Node cur = sentinal.next;
+        for(int i=0;i<=index;++i){
+            cur=cur.next;
+        }
+        return cur.item;
+    }
+}
